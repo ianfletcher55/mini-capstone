91e3fb7b5e768393afe2faab66f80676229bc27f
ianfletcher55 <ian.fletcher55@gmail.com>
2020-05-27 12:26:52 -0500

    adds route to all_products JSON

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,7 @@
 class Api::ProductsController < ApplicationController
+
+  def all_products_action
+    @all_products = Product.all
+    render json: @all_products
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/products" => "products#all_products_action"
+  end
+
 end
