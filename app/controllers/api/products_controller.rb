2c864cdc20577b36a17a4bcb30235e2b3919071a
ianfletcher55 <ian.fletcher55@gmail.com>
2020-05-27 12:38:24 -0500

    adds individual pages for first 3 products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,4 +4,20 @@ class Api::ProductsController < ApplicationController
     @all_products = Product.all
     render json: @all_products
   end
+
+  def get_guitar_strings
+    @guitar_strings = Product.find_by(id: 1)
+    render json: @guitar_strings
+  end
+
+  def get_guitar_case
+    @guitar_case = Product.find_by(id: 2)
+    render json: @guitar_case
+  end
+
+  def get_jazzmaster_guitar
+    @jazzmaster_guitar = Product.find_by(id: 3)
+    render json: @jazzmaster_guitar
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/products" => "products#all_products_action"
+
+    get "/guitar_strings" => "products#get_guitar_strings"
+
+    get "/guitar_case" => "products#get_guitar_case"
+
+    get "/jazzmaster_guitar" => "products#get_jazzmaster_guitar"
+
   end
 
 end
